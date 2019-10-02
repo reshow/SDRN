04eccf0b81ba4be4def4b9f9f7618e85a4d5d033
reshow <rzy503099765@qq.com>
2019-10-03 03:38:17 +0800

    speed up attention mask

--- attention.py
+++ attention.py
@@ -2,20 +2,26 @@ import numpy as np
 import numba
 from torchdata import UVmap2Mesh, uv_kpt, bfm2Mesh, getLandmark, mesh2UVmap, bfm, face_mask_np
 from faceutil import mesh
+import cv2
+
+face_mask_np3d = np.stack([face_mask_np, face_mask_np, face_mask_np], axis=2)
 
 
-@numba.jit
 def getImageAttentionMask(image, posmap, mode='hard'):
     """
     需要加一个正态分布吗？
     """
     [height, width, channel] = image.shape
-    mask = np.zeros((height, width)).astype(np.uint8)
-    for i in range(height):
-        for j in range(width):
-            [x, y, z] = posmap[i, j]
-            x = int(x)
-            y = int(y)
-            mask[y, x] = 1
+    p = (posmap * face_mask_np3d).clip(0, 255).astype(int)
+    mask = np.zeros((height, width))
+    # for i in range(height):
+    #     for j in range(width):
+    #         [x, y, z] = posmap[i, j]
+    #         x = int(x)
+    #         y = int(y)
+    #         mask[y, x] = 1
+    mask[p[:, :, 1], p[:, :, 0]] = 1
 
-    return mask.astype(np.uint8)
+    blur = cv2.GaussianBlur(mask, (5, 5), 0)
+    mask = np.ceil(np.array(blur)).astype(np.uint8)
+    return mask
